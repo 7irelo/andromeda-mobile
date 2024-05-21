879f64a2793696214b25c19a554bf62655617975
7irelo <155379151+7irelo@users.noreply.github.com>
2024-05-21 18:48:52 +0200

    babel

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js, jsx, ts, tsx}", "./components/**/*.{js, jsx, ts, tsx}"],
+  theme: {
+    extend: {
+      colours: {
+        primary: "#161622",
+        seondary: {
+          DEFAULT: "#FF9C01",
+          100: "#FF9001",
+          200: "#FF8E01"
+        },
+        black: {
+          DEFAULLT: "#000",
+          100: "#1E1E2D",
+          200: "#232533"
+        },
+        grey : {
+          100: "#CDCDE0",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
